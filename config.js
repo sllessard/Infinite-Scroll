20d3bf91052fc5c768b7b2755a5a9f68cc60a52f
Sheldon Lessard <lessard.sl@gmail.com>
2018-02-19 14:08:59 -0800

    Removed redundant files

--- config.js
+++
@@ -1,6 +0,0 @@
-	export const config = {
-		pagination: ' ', //Pagination ID
-		container: ' ', //Container for items
-		items: ' ', //Class for elements that will be appended
-		scrollThreshold: 0 //Optional
-	}
